1e29249621673e265e474e8f6d7af95d6569446f
Artem Shinkarenko <tomikus1@gmail.com>
2021-12-08 12:34:24 +0300

    fixed getCars for java 11

--- src/main/java/web/Service/CarServiceImpl.java
+++ src/main/java/web/Service/CarServiceImpl.java
@@ -1,8 +1,5 @@
 package web.Service;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import web.model.Car;
 
 import java.util.List;
@@ -10,6 +7,6 @@ import java.util.List;
 public class CarServiceImpl implements CarService {
     @Override
     public List<Car> getCars(int count, List<Car> cars) {
-        return cars.stream().limit(count).toList();
+        return cars.subList(0, Math.min(count, cars.size()));
     }
 }
